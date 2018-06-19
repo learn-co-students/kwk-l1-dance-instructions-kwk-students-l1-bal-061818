05645e52d00c24634819cb0fa4390a3f8761ed8a
Patricia <patchley02@gmail.com>
2018-06-19 13:52:44 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -2,7 +2,7 @@
 # Code your methods below
   def starting_stance
     puts "Plant legs far apart, bend knees slightly and keep posture loose"
-end 
+end
 def base_footwork
   puts "Lift right foot"
   puts "Return right foot to the ground"
@@ -18,21 +18,24 @@ def skip_step_right
   puts "Bounce right foot back up slightly, kicking it a few inches back"
   puts "Right, Left, Right and Right"
 end
-def bob_reins
+def bob_the_reins
   puts "Holding your arms out in front of you, straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+  puts "Now do this 7 more times!"
 end
 def lasso_step
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
   puts "Lift right arm up and point right elbow diagonally to the right."
   puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+  puts "Now do this 7 more times!"
 end
 
-
-starting_stance
-base_footwork
-skip_step_left
-skip_step_right
-bob_reins
-lasso_step
+def gangnam_style
+  starting_stance
+  base_footwork
+  skip_step_left
+  skip_step_right
+  bob_the_reins
+  lasso_step
+end
